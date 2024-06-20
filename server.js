9937f24805fc2e39583fbf6f4e4ca72ba1bfe797
Henrique Matheus <henrique.matheus.developer@gmail.com>
2024-06-20 12:24:29 -0300

    Atualização no server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ app.get('/', (req, res) => {
   res.send('Hello, World DIOGO PAZA!');
 });
 
-const server = app.listen(4000, () => {
-  console.log('Server listening on port 4000');
+const server = app.listen(8080, () => {
+  console.log('Server listening on port 8080');
 });
 
 setTimeout(() =>{    
